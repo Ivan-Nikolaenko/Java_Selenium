6e51f478de77a22e258c5e4b50ed3416bb9c3bb8
admin <Ivan-nikolaenko@bk.ru>
2023-07-18 22:27:10 +0600

    15 урок закончил со стрингой.

--- src/main/java/String15.java
+++ src/main/java/String15.java
@@ -7,5 +7,18 @@ public class String15 {
 
         System.out.println(s==s1);
         System.out.println(s.equals(s1));
+
+        String str ="My name %s! Im %d years old!";
+        int age = 30;
+        String name ="Ivan";
+
+        System.out.println(String.format(str,name,age));
+
+        String age2 = "30";
+        int a = Integer.parseInt(age2);
+        System.out.println(a*3);
+        System.out.println(age2.length());
+
+
     }
 }
